efb74217d9cb234439fb19cc0b59547dd1e0a065
DC <eopoxf@gmail.com>
2020-02-13 16:08:16 +0530

    Revert back the report time to 1hour

--- aurelia_project/environments/prod.js
+++ aurelia_project/environments/prod.js
@@ -20,7 +20,7 @@ export default {
   },
   petabencana: {
     title: 'Petabencana.id',
-    report_timeperiod: 86400,
+    report_timeperiod: 3600,
     default_language: 'id',
     tile_layer: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
     data_server: 'https://data.petabencana.id/',
