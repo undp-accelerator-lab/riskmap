716f9668687cd07c4ed347ff01d77ccfdaf0b60e
DC <eopoxf@gmail.com>
2020-05-09 02:45:51 +0530

    Update toast message

--- deployment_specific/pb/ds_locales/id.js
+++ deployment_specific/pb/ds_locales/id.js
@@ -14,7 +14,7 @@ const id = {
   'volcano': 'Gunung Api',
   'comming_soon': 'Segera Hadir',
   'coming_soon_2020': 'Segera Hadir!',
-  'reports_stats': 'Terdapat {reportsplaceholder} laporan dalam {hoursplaceholder} jam terakhir',
+  'reports_stats': 'Terdapat {reportsplaceholder} laporan dalam {hoursplaceholder} jam terakhir di {provinceplaceholder}',
   'prep_menu': {
     'btn_names': {
       'drain': 'Blocked Drains',
