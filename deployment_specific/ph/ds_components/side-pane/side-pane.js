fad617c3cade74b72973af063ec32150e806787a
Deepthi Chand <eopoxf@gmail.com>
2020-09-07 15:49:03 +0530

    update language for youtube

--- deployment_specific/ph/ds_components/side-pane/side-pane.js
+++ deployment_specific/ph/ds_components/side-pane/side-pane.js
@@ -40,7 +40,7 @@ export class SidePane {
 
     this.vidWrapperOpened = true;
     this.youtube_video = {
-      id: 'https://www.youtube.com/embed/vGiz7gjmo6c',
+      tl: 'https://www.youtube.com/embed/vGiz7gjmo6c',
       en: 'https://www.youtube.com/embed/vGiz7gjmo6c?autoplay=0&origin=https://mapakalamidad.ph&rel=0',
       icon: 'deployment_specific/pb/ds_assets/icons/youtube.png'
     }
