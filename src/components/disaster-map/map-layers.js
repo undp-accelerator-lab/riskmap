1f90f31a1a65055e9f5eb4b2091a1fc31c319e99
unknown <eopoxf@gmail.com>
2021-12-16 12:16:01 +0530

    update report summary url

--- src/components/disaster-map/map-layers.js
+++ src/components/disaster-map/map-layers.js
@@ -182,7 +182,7 @@ export class MapLayers {
     let self = this;
     let client = new HttpClient();
     const url = self.config.data_server +
-      'reportsSummary?admin=' + regionCode;
+      'stats/reportsSummary?admin=' + regionCode;
       // + '&timeperiod=' + self.config.report_timeperiod;
     return new Promise((resolve, reject) => {
       client.get(url)
