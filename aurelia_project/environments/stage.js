337c25c46769d2d61925b9d4cec23d152e7ff32d
ruthvik129 <ruthvik129@gmail.com>
2022-03-15 13:02:49 +0530

    Added slash to cognicity server

--- aurelia_project/environments/stage.js
+++ aurelia_project/environments/stage.js
@@ -21,7 +21,7 @@ export default {
     title: 'Petabencana.id',
     default_language: 'id',
     tile_layer: 'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
-    data_server: 'https://data-dev.petabencana.id',
+    data_server: 'https://data-dev.petabencana.id/',
     data_server_key: '{data_server_key}',
     cards_server: 'https://cards-dev.petabencana.id/',
     app: 'https://dev.petabencana.id/',
