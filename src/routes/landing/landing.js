5a59d312776a0017a230101535571f8a3bf9600a
ruthvik129 <ruthvik129@gmail.com>
2022-04-05 21:07:42 +0530

    added log to check the error

--- src/routes/landing/landing.js
+++ src/routes/landing/landing.js
@@ -1,15 +1,13 @@
-import $ from 'jquery';
-import {Config} from '../../resources/config';
-import { bindable, customElement } from 'aurelia-framework';
-import { inject, observable } from 'aurelia-framework';
-import { HttpClient } from 'aurelia-http-client';
+import $ from "jquery";
+import { Config } from "../../resources/config";
+import { bindable, customElement } from "aurelia-framework";
+import { inject, observable } from "aurelia-framework";
+import { HttpClient } from "aurelia-http-client";
 
 //start-aurelia-decorators
-@customElement('landing')
-
+@customElement("landing")
 @inject(Config, HttpClient)
 //end-aurelia-decorators
-
 export class Landing {
   //start-aurelia-decorators
   @bindable
@@ -25,14 +23,14 @@ export class Landing {
   constructor(Config) {
     this.config = Config.map;
     this.configData = Config;
-    this.activeDisaster = 'none';
+    this.activeDisaster = "none";
     this.assetMap = {
-      '#flood': '/assets/icons/Add_Report_Icon_Flood',
-      '#earthquake': '/assets/icons/Add_Report_Icon_Earthquake',
-      '#fire': '/assets/icons/Add_Report_Icon_Fire',
-      '#volcano': '/assets/icons/Add_Report_Icon_Volcano',
-      '#wind': '/assets/icons/wind',
-      '#haze': '/assets/icons/Add_Report_Icon_Haze'
+      "#flood": "/assets/icons/Add_Report_Icon_Flood",
+      "#earthquake": "/assets/icons/Add_Report_Icon_Earthquake",
+      "#fire": "/assets/icons/Add_Report_Icon_Fire",
+      "#volcano": "/assets/icons/Add_Report_Icon_Volcano",
+      "#wind": "/assets/icons/wind",
+      "#haze": "/assets/icons/Add_Report_Icon_Haze",
     };
     this.isFloatingMenuActive = false;
     this.enableEq = true;
@@ -43,26 +41,35 @@ export class Landing {
   }
 
   domouseout() {
-    $('#floating_buttons_wrapper').css({'height': '85px', 'overflow': 'hidden'});
+    $("#floating_buttons_wrapper").css({ height: "85px", overflow: "hidden" });
   }
 
   domouseover() {
-    $('#floating_buttons_wrapper').css({'height': '600px', 'overflow': 'visible'});
+    $("#floating_buttons_wrapper").css({
+      height: "600px",
+      overflow: "visible",
+    });
   }
-
 
   activate(params, routerConfig) {
     this.queried_city = params.city;
     this.report_id = params.report;
-    this.queried_lang = (this.configData.supported_languages.indexOf(params.lang) > -1) ? params.lang : null;
-    this.queried_tab = (params.tab === 'info' || params.tab === 'map' || params.tab === 'report') ? params.tab : null;
-    this.queried_terms = (params.terms === 'u_a' || params.terms === 'p_p') ? params.terms : null;
+    this.queried_lang =
+      this.configData.supported_languages.indexOf(params.lang) > -1
+        ? params.lang
+        : null;
+    this.queried_tab =
+      params.tab === "info" || params.tab === "map" || params.tab === "report"
+        ? params.tab
+        : null;
+    this.queried_terms =
+      params.terms === "u_a" || params.terms === "p_p" ? params.terms : null;
   }
 
   queryChanged(newval, oldval) {
     this.searchText = newval;
     const map = Object.keys(this.config.instance_regions);
-    let newObj = map.filter(value => {
+    let newObj = map.filter((value) => {
       return value.indexOf(newval) != -1 ? value : null;
     });
     this.searchResult = newObj;
@@ -80,28 +87,27 @@ export class Landing {
 
   //report button on the map
   reportTab(event) {
-    $('#reportLink').toggle('slide');
+    $("#reportLink").toggle("slide");
   }
 
   hideShowBlackBg(command) {
-    if (command === 'show') {
-      $('#black_bg').css('display', 'block');
+    if (command === "show") {
+      $("#black_bg").css("display", "block");
     } else {
-      $('#black_bg').css('display', 'none');
+      $("#black_bg").css("display", "none");
     }
   }
 
-
   openClose(event) {
-    $('#reportData').show();
+    $("#reportData").show();
   }
 
   resizeSidePane() {
-    $('#sidePane').css({
-      'height': ($(window).height() - ($('#topBar').height() + 30)) + 'px'
+    $("#sidePane").css({
+      height: $(window).height() - ($("#topBar").height() + 30) + "px",
     });
-    $('#dropdown_city').css({
-      'height': ($(window).height() - ($('#topBar').height() + 30)) + 'px'
+    $("#dropdown_city").css({
+      height: $(window).height() - ($("#topBar").height() + 30) + "px",
     });
     // $('#floating_buttons_wrapper').css({
     //   'max-height': ($(window).height() - 10) + 'px'
@@ -121,8 +127,8 @@ export class Landing {
     $(window).resize(() => {
       this.resizeSidePane();
     });
-    if (!(/Mobi/.test(navigator.userAgent)) && !this.report_id) {
-      this.mapModel.togglePane('#sidePane', 'show', false);
+    if (!/Mobi/.test(navigator.userAgent) && !this.report_id) {
+      this.mapModel.togglePane("#sidePane", "show", false);
     }
   }
 
@@ -133,57 +139,67 @@ export class Landing {
   initiateReport(type) {
     return new Promise((resolve, reject) => {
       if (type) {
-        const client = new HttpClient()
-          .configure(x => {
-            x.withHeader('x-api-key', this.config.data_server_key);
-          });
-        const url = this.config.data_server +
-        'cards/';
+        const client = new HttpClient().configure((x) => {
+          x.withHeader("x-api-key", this.config.data_server_key);
+        });
+        const url = this.config.data_server + "cards/";
         const body = {
-          username: 'web_guest',
+          username: "web_guest",
           language: this.webMenu.currentLanguage,
-          network: 'website'
+          network: "website",
         };
 
-        client.post(url, body)
-          .then(result => {
+        client
+          .post(url, body)
+          .then((result) => {
             if (result.statusCode && result.statusCode === 200) {
               resolve(JSON.parse(result.response).cardId);
             } else {
               reject(result);
             }
           })
-          .catch(error => reject(error));
+          .catch((error) => reject(error));
       } else {
-        reject('Error with report id');
+        reject("Error with report id");
       }
     });
   }
 
   reportDisaster(type) {
     let self = this;
-    self.initiateReport(type).then(cardId => {
-      window.location = self.config.cards_server + cardId  + '/' + type;
-    });
+    self
+      .initiateReport(type)
+      .then((cardId) => {
+        window.location = self.config.cards_server + cardId + "/" + type;
+      })
+      .catch((err) => {
+        console.log(
+          "🚀 ~ file: landing.js ~ line 168 ~ Landing ~ self.initiateReport ~ err",
+          err
+        );
+        alert(`Error is ${err}`);
+      });
   }
 
   selectReport(typeReport) {
     let self = this;
-    let toggleSrc = function(element) {
-      let extention = element === '#wind' ? '.svg' : '.png';
-      if (self.activeDisaster !== element ) {
+    let toggleSrc = function (element) {
+      let extention = element === "#wind" ? ".svg" : ".png";
+      if (self.activeDisaster !== element) {
         toggleSrc(self.activeDisaster);
         self.activeDisaster = element;
       } else {
-        self.activeDisaster = 'noactive';
+        self.activeDisaster = "noactive";
       }
-      $(element).attr('src', function(index, attr) {
+      $(element).attr("src", function (index, attr) {
         let baseIcon = self.assetMap[element];
 
-        return attr === baseIcon + extention ? baseIcon + '_Hover' + extention : baseIcon + extention;
+        return attr === baseIcon + extention
+          ? baseIcon + "_Hover" + extention
+          : baseIcon + extention;
       });
-      $(element + 'Link').toggle('slide');
+      $(element + "Link").toggle("slide");
     };
-    toggleSrc('#' + typeReport);
+    toggleSrc("#" + typeReport);
   }
 }
