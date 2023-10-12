ab8d6385c1fc64a50e2f602e8e3be3e0304c4d10
ruthvik129 <ruthvik129@gmail.com>
2023-10-12 15:32:03 +0530

    increase the button size and recenter the map view

--- aurelia_project/deployments/pa.js
+++ aurelia_project/deployments/pa.js
@@ -10,7 +10,7 @@ export default {
       'Panama': {
         'region': '08',
         'bounds': { 'sw': [-79.70, 8.18], 'ne': [-78.06, 9.50] },
-        'center': [9.10, -78.92]
+        'center': [9.10, -79.51]
       }
     },
     'default_region': {
@@ -19,15 +19,15 @@ export default {
         'sw': [-79.70, 8.18], // [ymin , xmin]
         'ne': [-78.06, 9.50] // [ymax, xmax]
       },
-      'center': [9.10, -78.92]
+      'center': [9.10, -79.51]
     },
     'initial_load': [],
     'sub_regions': {
       'Panama': { 'province': 'Panama', 'center': [9.09, -79.42] },
       'san miguelito': { 'province': 'Panama', 'center': [9.05, -79.49] }
     },
-    'region_center': [9.10, -78.92],
-    'start_city_center': [9.10, -78.92],
+    'region_center': [9.10, -79.51],
+    'start_city_center': [9.10, -79.51],
     'starting_zoom': 8.75,
     'minimum_zoom': 8
   }
